0f3dfe0bba3739b39ffcccf7a951ba0179406f5f
viktoriamastrenko <victoriamastrenko@gmail.com>
2021-09-17 16:01:55 -0500

    Added calculation of mortgage

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,14 +14,44 @@ function solveEquation(a, b, c) {
     const x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
-  
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount = null;
+  const params = [
+    {name: "Процентная ставка", value: percent },
+    {name: "Начальный взнос", value: contribution },
+    {name: "Общая стоимость", value: amount }
+  ];
 
-  // код для задачи №2 писать здесь
+  for (let param of params) {
+    if (checkValidity(param) !== true) {
+      return `Параметр "${param.name}" содержит неправильное значение "${param.value}"`;
+    }
+  }
 
-  return totalAmount;
+  const creditSum = +amount - +contribution;
+  const now = new Date();
+  const months = monthDiff(now, date);
+  const p = (+percent / 100) / 12;
+  const monthlyPayment = creditSum * (p + p/ (((1 + p)**months) - 1));
+  totalAmount = (monthlyPayment * months).toFixed(2);
+
+  return +totalAmount;
+}
+
+function checkValidity(param) {
+    if (!isFinite(param.value) || param.value === "" || +param.value < 0) {
+      return false;
+    }
+    return true;
+}
+
+function monthDiff(date1, date2) {
+  let months = (date2.getFullYear() - date1.getFullYear()) * 12;
+  months -= date1.getMonth();
+  months += date2.getMonth();
+  return Math.abs(months);
 }
