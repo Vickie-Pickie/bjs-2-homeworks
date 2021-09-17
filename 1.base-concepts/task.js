40f43ff2ee4041c696c38da166791a8e6ee90e11
viktoriamastrenko <victoriamastrenko@gmail.com>
2021-09-17 13:18:36 -0500

    Finding square roots

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  const d = b**2 - 4 * a * c;
+
+  if (d === 0) {
+    const x = -b / (2 * a);
+    arr.push(x);
+  }
+
+  if (d > 0) {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  
   return arr; // array
 }
 
