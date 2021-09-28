45497a6fa97787be3dad25ec88aaf1ae6564cb3f
viktoriamastrenko <victoriamastrenko@gmail.com>
2021-09-28 14:19:12 -0500

    added compareArrays, dvancedFilter

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  result = (arr1.length === arr2.length) && arr1.every(function(elem, ind) {
+      return elem === arr2[ind];
+    });
 
   return result; // boolean
 }
@@ -9,7 +10,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(item => item > 0 && (item % 3 == 0)).map(item => item * 10);
 
   return resultArr; // array
 }
