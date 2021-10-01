c060415e8e1539cd4e9ef8ba1dd098c2cac48cf3
viktoriamastrenko <victoriamastrenko@gmail.com>
2021-10-01 15:36:41 -0500

    Added Student funtion constructor

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [];
+    }
+
+    this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+        this.marks = [];
+    }
+
+    for (let mark of marks) {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    const length = this.marks.length;
+    for (let mark of this.marks) {
+        sum += mark;
+    }
+
+    return sum / length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
+}
